1db149fd5be668bf9c13fc871f8df521048d8f9d
YuriToribio <yuritoribio@gmail.com>
2022-10-11 15:59:16 -0300

    Alterações de ajuste dos nomes das pastas

---
+++ Fundamentos/secao2-Git-GitHub-Internet/dia2-Git-e-github-entendendo-comandos/Exercicios-agora-a-pratica.sh
@@ -0,0 +1,16 @@
+#Crie uma nova branch
+
+git checkout -b exercicios-git
+git checkout exercicios-git
+
+cd Fundamentos
+cd Bloco2-Git-Github-Internet
+cd Dia2-Git-e-github-entendendo-comandos
+
+touch README.md
+git add .
+git commit -m 'Adicionando um readme na pasta de exercicios Git - dia 2'
+
+git push
+
+#O pull request é feito no site do github
