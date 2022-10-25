587837c0678d809ac3505b97b4bc989d636cccaf
YuriToribio <yuritoribio@gmail.com>
2022-10-25 16:59:10 -0300

    Adicionando as primeitas funções do exercicio 7

---
+++ Fundamentos/secao4-Introducao-a-javascript/Dia1-Primeiros-passos/exercicio7.js
@@ -0,0 +1,34 @@
+// Criação das Funções //
+function adicao (a, b){
+    return(a+b);
+}
+
+function subtracao(a, b){
+    return(a-b);
+}
+
+function multiplicacao(a, b){
+    return (a*b);
+}
+
+function divisao(a, b){
+    return (a/b);
+}
+
+function resto(a, b){
+    return (a%b);
+}
+
+// Aplicação das Funções //
+
+let num1=10;
+let num2=5;
+
+console.log(adicao(num1, num2));
+console.log(subtracao(num1, num2));
+console.log(multiplicacao(num1, num2));
+console.log(divisao(num1, num2));
+console.log(resto(num1, num2));
+
+/////////////////////////////////////////////////////
+
