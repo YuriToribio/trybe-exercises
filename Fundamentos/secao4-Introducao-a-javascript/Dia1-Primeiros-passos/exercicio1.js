7082712fbf6a89c8c9428499a94270a7a9ec3b3d
YuriToribio <yuritoribio@gmail.com>
2022-10-25 16:10:05 -0300

    Adicionando o exercicio 1

---
+++ Fundamentos/secao4-Introducao-a-javascript/Dia1-Primeiros-passos/exercicio1.js
@@ -0,0 +1,11 @@
+const myName = 'Yuri';
+const birthCity= 'Guarapari';
+let birthYear = 1990;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthCity = 'Belo horizonte';
+
+// Variáveis do tipo const não podem ser alteradas.
