09709b8b74a6e6860b15b17d37d7a7ab76642c7e
YuriToribio <yuritoribio@gmail.com>
2022-10-25 16:15:06 -0300

    Adicionando o segundo exercicio

---
+++ Fundamentos/secao4-Introducao-a-javascript/Dia1-Primeiros-passos/exercicio2.js
@@ -0,0 +1,12 @@
+// -- Exercicio 2 --
+
+const base = 5;
+const heigth = 8;
+
+const area= base*heigth;
+const perimeter=(2*base)+(2*heigth);
+
+console.log(base);
+console.log(heigth);
+console.log(area);
+console.log(perimeter);
