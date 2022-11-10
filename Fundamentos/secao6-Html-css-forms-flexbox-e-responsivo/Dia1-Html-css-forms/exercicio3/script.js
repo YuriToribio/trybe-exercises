8b8d4c11a6fd714893fc7817752a6fdd27276b3c
YuriToribio <yuritoribio@gmail.com>
2022-11-10 20:44:18 -0300

    Exercicios do Dia 1

---
+++ Fundamentos/secao6-Html-css-forms-flexbox-e-responsivo/Dia1-Html-css-forms/exercicio3/script.js
@@ -0,0 +1,24 @@
+const submitDefaultprevent = () => {
+    let botaoSubmit = document.querySelector('#botao-submit');
+    botaoSubmit.addEventListener('click', (event) => {
+        event.preventDefault();
+    })
+}
+window.onload = submitDefaultprevent ();
+
+function limparFormulario() {
+    let botaoLimpar = document.querySelector('#limpar');
+    botaoLimpar.addEventListener('click', () => {
+        const formElements = document.querySelectorAll('input');
+        const textArea = document.querySelector('textarea');
+        for (let index = 0; index < formElements.length; index += 1) {
+            const userInput = formElements[index];
+            userInput.value = '';
+            userInput.checked = false;
+        }
+        textArea.value = '';
+    })
+}
+limparFormulario();
+
+  
---
+++ Fundamentos/secao6-Html-css-forms-flexbox-e-responsivo/Dia1-Html-css-forms/exericicio2/script.js
@@ -0,0 +1,22 @@
+// SELETORES
+const INPUT_TEXT = document.querySelector("#input-text");
+const INPUT_CHECKBOX = document.querySelector("#input-checkbox");
+const HREF_LINK = document.querySelector("#href");
+
+HREF_LINK.addEventListener('click', (evento) => {
+    evento.preventDefault();
+});
+
+INPUT_CHECKBOX.addEventListener('click', (evento) => {
+    evento.preventDefault();
+});
+
+INPUT_TEXT.addEventListener('keypress', (evento) => {
+    
+    const character = evento.key;
+  
+    if (character !== 'a') {
+      evento.preventDefault();
+    }
+  
+});
